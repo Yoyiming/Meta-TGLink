42d1b5dfc0edebb264e9ae51c83dba90263385ca
Yoyiming <yoyiming7@gmail.com>
2025-09-07 17:41:00 +0800

    Upload datasets and the generation codes of all datasets

---
+++ limma_dataset_generate.py
@@ -0,0 +1,98 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import random
+import sys
+
+# random_seed = sys.argv[1]
+random_seed = 999
+# sample = sys.argv[2]
+sample = 'sample5'
+
+def generate_gene_index_df(expression_matrix_path):
+    expression_matrix = pd.read_csv(expression_matrix_path, index_col=0)
+    gene_names = expression_matrix.index.tolist()
+    gene_index_df = pd.DataFrame({'Gene': gene_names, 'Index': range(len(gene_names))})
+    return gene_index_df
+
+
+def create_network_with_labels(network_path, gene_index_df):
+    network_df = pd.read_csv(network_path)
+    network_df['Label'] = 1
+    tf_names = network_df['TF'].unique()
+
+    all_genes = set(gene_index_df['Gene'].tolist())
+    tf_target_pairs = network_df.groupby('TF')['Target'].apply(set).to_dict()
+
+    negative_samples = []
+    for tf, targets in tf_target_pairs.items():
+        non_targets = list(all_genes - targets - {tf})
+        num_positive_samples = len(targets)
+        if num_positive_samples > len(non_targets) / 2:
+            sampled_non_targets = non_targets
+        else:
+            sampled_non_targets = np.random.choice(non_targets, num_positive_samples, replace=False)
+        for nt in sampled_non_targets:
+            negative_samples.append({'TF': tf, 'Target': nt, 'Label': 0})
+
+    negative_samples_df = pd.DataFrame(negative_samples)
+    complete_network_df = pd.concat([network_df, negative_samples_df])
+
+    return complete_network_df
+
+
+def split_and_save_datasets(complete_network_df, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    train_df = pd.DataFrame()
+    val_df = pd.DataFrame()
+    test_df = pd.DataFrame()
+
+    tfs = complete_network_df['TF'].unique()
+    for tf in tfs:
+        tf_df = complete_network_df[complete_network_df['TF'] == tf]
+        positive_samples = tf_df[tf_df['Label'] == 1]
+        negative_samples = tf_df[tf_df['Label'] == 0]
+
+        if len(positive_samples) <= 10:
+            train_df = pd.concat([train_df, tf_df])
+            continue
+        
+        pos_train, pos_temp = train_test_split(positive_samples, test_size=0.4, random_state=random_seed)
+        pos_val, pos_test = train_test_split(pos_temp, test_size=0.5, random_state=random_seed)
+
+        neg_train, neg_temp = train_test_split(negative_samples, test_size=0.4, random_state=random_seed)
+        neg_val, neg_test = train_test_split(neg_temp, test_size=0.5, random_state=random_seed)
+
+        tf_train_df = pd.concat([pos_train, neg_train])
+        tf_val_df = pd.concat([pos_val, neg_val])
+        tf_test_df = pd.concat([pos_test, neg_test])
+
+        train_df = pd.concat([train_df, tf_train_df])
+        val_df = pd.concat([val_df, tf_val_df])
+        test_df = pd.concat([test_df, tf_test_df])
+
+    train_df.to_csv(os.path.join(output_dir, f'{sample}_train.csv'), index=False)
+    val_df.to_csv(os.path.join(output_dir, f'{sample}_val.csv'), index=False)
+    test_df.to_csv(os.path.join(output_dir, f'{sample}_test.csv'), index=False)
+
+
+def main(cell_line_name):
+    expression_matrix_path = f'expression matrix/oe/{cell_line_name}/limma_expression_matrix.csv'
+    network_path = f'indirected network/oe/{cell_line_name}/limma_networks.csv'
+    output_dir = f'cell_line_dataset/oe/{cell_line_name}'
+
+    gene_index_df = generate_gene_index_df(expression_matrix_path)
+    complete_network_df = create_network_with_labels(network_path, gene_index_df)
+    split_and_save_datasets(complete_network_df, output_dir)
+
+    # gene_index_df.to_csv(os.path.join(output_dir, 'Genes.csv'), index=False)
+
+
+if __name__ == '__main__':
+    cell_line_name = ['A375', 'A549', 'HEK293T', 'PC3', 'MCF7']
+    for cell_line in cell_line_name:
+        main(cell_line)
+    print('Done')
---
+++ tf_dataset_split.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import random
+
+random.seed(2024)
+
+cell_lines = ['A549', 'A375', 'HEK293T', 'PC3', 'MCF7']
+samples = ['sample1', 'sample2', 'sample3', 'sample4', 'sample5']
+for sample in samples:
+    for cell_line in cell_lines:
+        train_data_path = f'cell_line_dataset/oe/{cell_line}/{sample}_train.csv'
+        val_data_path = f'cell_line_dataset/oe/{cell_line}/{sample}_val.csv'
+        test_data_path = f'cell_line_dataset/oe/{cell_line}/{sample}_test.csv'
+
+        train_data = pd.read_csv(train_data_path)
+        val_data = pd.read_csv(val_data_path)
+        test_data = pd.read_csv(test_data_path)
+
+        dataset = pd.concat([train_data, val_data, test_data])
+        tf_num = dataset['TF'].unique().shape[0]
+        tf_list = dataset['TF'].unique().tolist()
+        # 打乱顺序
+        random.shuffle(tf_list)
+
+        train_num = int(tf_num * 0.6)
+        val_num = int(tf_num * 0.2)
+        test_num = tf_num - train_num - val_num
+
+        train_sample_tfs = tf_list[:train_num]
+        val_sample_tfs = tf_list[train_num:train_num+val_num]
+        test_sample_tfs = tf_list[train_num+val_num:]
+
+        train_data_new = dataset[dataset['TF'].isin(train_sample_tfs)]
+        val_data_new = dataset[dataset['TF'].isin(val_sample_tfs)]
+        test_data_new = dataset[dataset['TF'].isin(test_sample_tfs)]
+
+        train_data_new.to_csv(f'cell_line_dataset/oe/{cell_line}/{sample}_tf_train.csv', index=False)
+        val_data_new.to_csv(f'cell_line_dataset/oe/{cell_line}/{sample}_tf_val.csv', index=False)
+        test_data_new.to_csv(f'cell_line_dataset/oe/{cell_line}/{sample}_tf_test.csv', index=False)
+
+print('Done!')
